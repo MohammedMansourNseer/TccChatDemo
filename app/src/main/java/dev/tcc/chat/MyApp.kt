9c3ac7ed2e4ce6f620f0bd301a0455b2ed41364a
LENOVO <24920915+MohammedMansourNseer@users.noreply.github.com>
2025-10-21 16:33:59 +0300

    1- handle Manifest and app class 2- handle theme

---
+++ app/src/main/java/dev/tcc/chat/MyApp.kt
@@ -0,0 +1,9 @@
+package dev.tcc.chat
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltAndroidApp
+class MyApp :Application() {
+}
